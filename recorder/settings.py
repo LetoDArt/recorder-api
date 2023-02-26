cdf3008d97fa13afee2358b3d011364a88b2c156
Leto D-Art <goncharoff.k2018@yandex.ru>
2023-02-26 14:59:20 +0300

    initialized django application

--- recorder/settings.py
+++ recorder/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oeu&81#d_hi#f_4*8exihld@cbl=ymz*_nq==d8r=l5x89u9g^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
